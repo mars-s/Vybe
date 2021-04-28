b51ee3a75085436cf77304aac2fcf1bcc6066432
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-28 16:16:02 +1000

    Added chat signUp and login components

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,10 @@
+
+const Chat = () => {
+  return (
+    <div>
+      i am the chat
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/components/SignUp/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      i am signup
+    </div>
+  )
+}
+
+export default Signup
