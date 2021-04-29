6eeb0556de9a18fd2a15f86034112b5294f55975
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-04-29 19:15:12 +1000

    Tried fixing #2
    While I tried fixing it, new errors came up regarding the chat, login and signup page I fixed that by separating the imports in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
-import { Route, Switch } from 'react-router-dom'
-import { Login, Signup, Chat } from 'components'
+import { Route, Switch } from 'react-router-dom';
+import Signup from 'components';
+import Chat from 'components';
+import Login from 'components';
 
 const App = () => {
   return (
