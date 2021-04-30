ea67b7b70ec22e865ec5217cb4288bca069e1dfe
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-30 21:19:21 +1000

    Created a sign up page

--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Formik, Form, ErrorMessage, Field } from "formik"
+import { useHistory } from 'react-router-dom'
+import { validationSchema, defaultValues } from './formikConfig'
+import { FormField } from 'components'
 
 export const Signup = () => {
+  const history = useHistory()
+
   return (
-    <div>
-      i am signup
+    <div className='auth-form'>
+      <h1>Sign Up</h1>
+      <Formik
+        onSubmit={() => console.log('submit')}
+        validatorOnMount={true}
+        initialValues={defaultValues}
+        validationSchema={validationSchema}
+      >
+        {/* where the actual html of the website goes */}
+        {({ isValid, isSubmitting }) => (
+          <Form>
+            <FormField name='email' label='E-Mail' type='email' />
+            <FormField name='userName' label='Username' />
+            <FormField name='password' label='Password' type='password' />
+            <FormField type='password' name='verifyPassword' label='Confirm Password' />
+
+            <button disabled={isSubmitting || !isValid} type='submit'>Sign Up</button>
+
+            <div className="auth-link-container">
+              Already have an account?{' '}
+              <span className="auth-link" onClick={() => history.push('login')}>
+                Log In!
+              </span>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
