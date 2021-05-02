a953723624283cda5f3452adfecea370b74fe692
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-05-02 18:09:34 +1000

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,18 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import { Login, Signup, Chat } from 'components';
-import { useAuth } from 'hooks'
+import { useAuth, useResolved } from 'hooks'
 import { useEffect } from 'react';
 
 export const App = () => {
-
+  const history = useHistory()
   const { authUser } = useAuth()
+  const authResolved = useResolved(authUser)
 
   useEffect(() => {
-    console.log('AUTH USER:', authUser)
-  }, [authUser])
+    if (authResolved) {
+      history.push(!!authUser ? '/' : '/login')
+    }
+  }, [authResolved, authUser, history])
 
   return (
     <div className='app'>
